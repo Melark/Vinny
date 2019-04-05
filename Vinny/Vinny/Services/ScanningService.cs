12752397ae6710c3ac771a5573067a9193ce7a08
Mark Arrow <markarrow6@gmail.com>
2019-04-05 13:26:50 +0200

    Scanner Service Implementation

---
+++ Vinny/Vinny/Services/ScanningService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Vinny.Interfaces;
+using ZXing.Mobile;
+
+namespace Vinny.Services
+{
+    public class ScanningService : IScannerService
+    {
+        public async Task<string> ScanBarcode()
+        {
+            var scanner = new MobileBarcodeScanner();
+            var result = await scanner.Scan();
+            return result?.Text ?? string.Empty;
+        }
+    }
+}
